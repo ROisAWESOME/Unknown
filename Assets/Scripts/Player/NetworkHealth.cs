08c74038668c38980397006751d9fec1a7a58a2b
ROBdk97 <robletsplays1@googlemail.com>
2017-05-14 22:17:51 +0200

    A

---
+++ Assets/Scripts/Player/NetworkHealth.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+[NetworkSettings(channel = 1)]
+public class NetworkHealth : NetworkBehaviour
+{
+	[SyncVar]
+    [Range(0, 100)]
+	public float Health = 100f;
+	[SyncVar]
+	public float deaths = 0f;
+	[SyncVar]
+	public bool dead = false;
+	public GameObject RespawnB;
+	public GameObject UI;
+	public Text infoMessageText;
+
+
+    //TODO: [Server]
+    public void GetShot(int damage)
+    {
+        Health -= (damage*((100-gameObject.GetComponent<PlayerInfo>().amor())/100));
+        RpcUpdateHealth(Health);
+    }
+	//TODO: [Server]
+	public void GetHeald (float healing){
+		Health += healing;
+		RpcUpdateHealth (Health);
+	}
+
+
+    [ClientRpc(channel = 1)]
+    private void RpcUpdateHealth(float health)
+    {
+        Health = health;
+    }
+
+	void Update(){
+		if(Health<=0f&&dead==false){
+			dead=true;
+			print("Somebody died!");
+			CmdDead ();
+			GameObject player = NetworkServer.FindLocalObject(gameObject.GetComponent<NetworkIdentity>().netId);
+			player.GetComponent<Collider>().enabled = false;
+			//gameObject.GetComponent<FirstPersonController>().enabled = false;
+			//player.GetComponent<NetworkGun>().enabled = false;
+			RespawnB.SetActive(true);
+			UI.SetActive(false);
+			deaths++;
+			//player.GetComponent<Screen>().screenLock=false;
+
+		}
+		if(Health>100){
+			Health = 100;
+		}
+	}
+
+	public void Respawn(){
+		GameObject player = NetworkServer.FindLocalObject(gameObject.GetComponent<NetworkIdentity>().netId);
+		RespawnB.SetActive(false);
+		UI.SetActive(true);
+		player.transform.position =  GameObject.Find ("Spawn").gameObject.transform.position;
+		player.GetComponent<Collider>().enabled = true;
+		//gameObject.GetComponent<FirstPersonController>().enabled = true;
+		//player.GetComponent<NetworkGun>().enabled = true;
+		Health=100f;
+		RpcUpdateHealth (Health);
+		dead = false;
+		//player.GetComponent<Screen>().screenLock=true;
+	}
+
+	[Command(channel = 1)]
+	public void CmdDead(){
+		RpcDead ();
+	}
+
+	[ClientRpc(channel = 1)]
+	public void RpcDead(){
+		infoMessageText.text = (infoMessageText.text+"\r\n"+gameObject.name+" has Died!");
+	}
+
+
+
+}
---
+++ Assets/Scripts/Player/NetworkPlayer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+
+    public GameObject Camera;
+    public GameObject Head;
+
+
+    [HideInInspector]
+    public NetworkMovement movement;
+    [HideInInspector]
+    public NetworkHealth health;
+	public Transform weaponSpot;
+
+    //[HideInInspector]
+    //public CharacterController CharacterController;
+
+    void Awake()
+    {
+        movement = GetComponent<NetworkMovement>();
+        health = GetComponent<NetworkHealth>();
+		//Options opt = GameObject.Find ("Options").GetComponent<Options> ();
+		//if (!String.Equals(opt.name, "")) {
+		//	gameObject.name = opt.nameOfPlayer;
+		//} else {
+		//	gameObject.name ="Player";
+		//}
+		//GameObject weaponO = Instantiate (opt.weapons [opt.weapon], weaponSpot.position, Quaternion.identity)as GameObject;
+		//weaponO.transform.SetParent (GetComponentInChildren<Camera>().gameObject.transform);
+		//weaponO.transform.rotation=Quaternion.Euler(270,0,90);
+		//gameObject.GetComponent<NetworkGun> ().WeaponUpdateInfo ();
+    }
+
+    void Start()
+    {	
+
+		//CharacterController = this.GetComponent<CharacterController> ();
+		
+
+        this.GetComponent<FirstPersonController>().enabled = isLocalPlayer;
+		//this.GetComponent<CharMovement>().enabled=isLocalPlayer;
+		//this.GetComponent<RigidbodyFirstPersonController> ().enabled = isLocalPlayer;
+		Camera.GetComponent<AudioListener>().enabled = isLocalPlayer;
+        Camera.GetComponent<Camera>().enabled = isLocalPlayer;
+
+    }
+
+
+
+}
